65564314878185c075813a7ae77a042ca6ac0792
maxispse <165880370+maxispse@users.noreply.github.com>
2025-12-01 10:31:09 +0100

    scene add
    new scenes, more grappling hook pain from Ondruš (i felt the pain too 😣)

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("NextScene");
+        }
+    }*/
+}
